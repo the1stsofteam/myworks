c9c22631ddf3d28e5e3c99670e4723f3160004a1
the1stsofteam <the1stsofteam@noreply.codeberg.org>
2023-03-04 03:09:39 +0000

    删除 'app/includes/db.php'

--- app/includes/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database connection
-if (ENABLE_DB) {
-	$db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
-	
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-}
